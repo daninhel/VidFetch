2d27bb2fb4edabba16e63ce0f030213444b039e6
daninkk <daniel.carachesque05@gmail.com>
2026-03-06 18:52:35 -0300

    fix(api): Implement Advanced Apple iOS User-Agent spoofing to bypass DataCenter Bot Walls

--- api/download.py
+++ api/download.py
@@ -34,7 +34,11 @@ def download():
         'no_warnings': True,
         'nocheckcertificate': True,
         'extractor_args': {
-            'youtube': ['player_client=ios,android']
+            'youtube': ['player_client=ios,tv', 'player_skip=webpage,configs']
+        },
+        'http_headers': {
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1',
+            'Accept-Language': 'en-US,en;q=0.9'
         }
     }
 
